bf471ea671b846b2d27b45ccaca03432e65a4739
H <huangsiyan1012@163.com>
2026-01-07 13:32:08 +0800

    ant design下载及使用

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { QuestionCircleOutlined } from "@ant-design/icons";
+import { Button } from "antd";
+
+import "antd/dist/antd.css";
+
 function App() {
-  return <div>app</div>;
+  return (
+    <div>
+      <QuestionCircleOutlined />
+      <Button type="primary">Primary Button</Button>
+    </div>
+  );
 }
 
 export default App;
