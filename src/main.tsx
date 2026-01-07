4325c69ef07f641864746b768adba4c8fb5d5c14
H <huangsiyan1012@163.com>
2026-01-07 12:53:36 +0800

    配置路径别名

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 // 初始化样式一般放在最前面
 import "reset-css";
 // 全局样式
-import "./assets/style/global.scss";
+import "@/assets/style/global.scss";
 
 import App from "./App.tsx";
 
