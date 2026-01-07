e07a41b311a7f3ea4a7de73868617cc41bd6c233
H <huangsiyan1012@163.com>
2026-01-07 11:28:19 +0800

    scss安装和基本使用

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+// 初始化样式一般放在最前面
 import "reset-css";
+// 全局样式
+import "./assets/style/global.scss";
+
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
